fca1ef79cda93aa4116c8f3cf6973374c8250019
Liam <liam@enclave.vn>
2017-10-10 18:41:02 +0700

    Change to PIC12F683 Sim

--- PIC12F675_PWM.X/nbproject/Makefile-local-default.mk
+++ PIC12F675_PWM.X/nbproject/Makefile-local-default.mk
@@ -14,23 +14,24 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-PATH_TO_IDE_BIN=/usr/microchip/mplabx/v4.00/mplab_ide/platform/../mplab_ide/modules/../../bin/
+SHELL=cmd.exe
+PATH_TO_IDE_BIN=D:/02_DevTools/MPLABX_3_61/mplab_ide/platform/../mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=/usr/microchip/mplabx/v4.00/mplab_ide/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=D:/02_DevTools/MPLABX_3_61/mplab_ide/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="/usr/microchip/mplabx/v4.00/sys/java/jre1.8.0_141/bin/"
+MP_JAVA_PATH="D:\02_DevTools\MPLABX_3_61\sys\java\jre1.8.0_121/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="/usr/microchip/xc8/v1.43/bin/xc8"
+MP_CC="D:\02_DevTools\MPLABX_XC8_142\bin\xc8.exe"
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="/usr/microchip/xc8/v1.43/bin/xc8"
-MP_LD="/usr/microchip/xc8/v1.43/bin/xc8"
+MP_AS="D:\02_DevTools\MPLABX_XC8_142\bin\xc8.exe"
+MP_LD="D:\02_DevTools\MPLABX_XC8_142\bin\xc8.exe"
 # MP_AR is not defined
-DEP_GEN=${MP_JAVA_PATH}java -jar "/usr/microchip/mplabx/v4.00/mplab_ide/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
-MP_CC_DIR="/usr/microchip/xc8/v1.43/bin"
+DEP_GEN=${MP_JAVA_PATH}java -jar "D:/02_DevTools/MPLABX_3_61/mplab_ide/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+MP_CC_DIR="D:\02_DevTools\MPLABX_XC8_142\bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="/usr/microchip/xc8/v1.43/bin"
-MP_LD_DIR="/usr/microchip/xc8/v1.43/bin"
+MP_AS_DIR="D:\02_DevTools\MPLABX_XC8_142\bin"
+MP_LD_DIR="D:\02_DevTools\MPLABX_XC8_142\bin"
 # MP_AR_DIR is not defined
 # MP_BC_DIR is not defined
